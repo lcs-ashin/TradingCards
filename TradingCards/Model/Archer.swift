6a6569e88805dd178c59a7c74b05ef04a2572eee
lcs-ashin <ashin@lcs.on.ca>
2021-11-10 14:10:17 -0500

    Added more "Archer"s in "listOfStats"

--- TradingCards/Model/Archer.swift
+++ TradingCards/Model/Archer.swift
@@ -12,15 +12,31 @@ struct Archer: Identifiable {
     let id = UUID()
     let archerName: String
     let imageName: String
-    let debutDate: Int
-    let age: Int
-    let bestScore: Int
-    let winningPercentage: Int
-    let worldRanking: Int
-    let averageScore: Double
+    let debutDate: String
+    let age: String
+    let bestScore: String
+    let winningPercentage: String
+    let worldRanking: String
+    let averageScore: String
 }
 
 let listOfStats = [
     
-    Archer(archerName: "An San", imageName: "An San", debutDate: 2019, age: 20, bestScore: 684, winningPercentage: 76, worldRanking: 1, averageScore: 9.4)
+    Archer(archerName: "An San", imageName: "An San", debutDate: "International Debut: 2019", age: "Age: 20", bestScore: "Best Score: 684/720", winningPercentage: "Winning Percentage: 76%", worldRanking: "World Ranking: 1", averageScore: "Average Score Per Arrow: 9.4")
+    
+    ,
+    
+    Archer(archerName: "Kim Woo-Jin", imageName: "Kim Woo Jin", debutDate: "International Debut: 2009", age: "Age: 29", bestScore: "Best Score: 700/720", winningPercentage: "Winning Percentage: 81%", worldRanking: "World Ranking: 3", averageScore: "Average Score Per Arrow: 9.6")
+    
+    ,
+    
+    Archer(archerName: "Oh Jin-Hyek", imageName: "Oh Jin Hyek", debutDate: "International Debut: 1999", age: "Age: 40", bestScore: "Best Score: 691/720", winningPercentage: "Winning Percentage: 74%", worldRanking: "World Ranking: 26", averageScore: "Average Score Per Arrow: 9.4")
+    
+    ,
+    
+    Archer(archerName: "Kim Je-Deok", imageName: "Kim Je Deok", debutDate: "International Debut: 2019", age: "Age: 17", bestScore: "Best Score: 688/720", winningPercentage: "Winning Percentage: 88%", worldRanking: "World Ranking: 50", averageScore: "Average Score Per Arrow: 9.5")
+    
+    ,
+    
+    Archer(archerName: "Kang Chae-Young", imageName: "Kang Chae Young", debutDate: "International Debut: 2015", age: "Age: 25", bestScore: "Best Score: 692/720", winningPercentage: "Winning Percentage: 85%", worldRanking: "World Ranking: 7", averageScore: "Average Score Per Arrow: 9.3")
 ]
