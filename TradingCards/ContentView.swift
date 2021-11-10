c38c46cff2444a97f56bdf5bafbb770be6e85566
lcs-ashin <ashin@lcs.on.ca>
2021-11-10 15:30:05 -0500

    Changed the simulator to iPhone 13 Pro Max (bigger screen) and replaced some "frames" to "paddings" to make it fit to most devices.

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -11,88 +11,91 @@ struct ContentView: View {
     let stat: Archer
     
     var body: some View {
-        VStack(alignment: .center, spacing: 5) {
-            
-            // Invisible rectangle
-            Rectangle()
-                .frame(width: 500, height: 20, alignment: .center)
-            // Name
-            Text(stat.archerName)
-                .font(Font.custom("Helvetica Neue", size: 40).bold().italic())
-            // Decoration
-            ZStack {
-                Rectangle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
-                    .frame(width: 450, height: 35, alignment: .topLeading)
-                Text("ARCHERY ARCHERY ARCHERY")
-                    .font(Font.custom("Avenir Next", size: 25).italic().bold())
-                    .foregroundColor(.white)
-            }
-            Spacer()
-            // Photo
-            ZStack {
-                Circle()
-                    .fill(Color.black)
-                    .frame(width: 390, height: 390, alignment: .center)
-                Circle()
-                    .fill(Color.blue)
-                    .frame(width: 370, height: 380, alignment: .center)
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 345, height: 370, alignment: .center)
-                Image(stat.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(Circle())
-                    .padding()
-            }
-            Spacer()
-            
-            HStack {
-                ZStack {
-                    // Background
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(Color.red)
-                        .frame(width: 350, height: 230, alignment: .topLeading)
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(Color.blue.opacity(0.55))
-                        .frame(width: 370, height: 250, alignment: .topLeading)
-                    // Stats
-                    VStack(alignment: .leading, spacing: 3) {
-                        Group {
-                            Text(stat.debutDate)
-                            Text(stat.age)
-                            Text(stat.bestScore)
-                            Text(stat.winningPercentage)
-                            Text(stat.worldRanking)
-                            Text(stat.averageScore)
-                        }
-                        .font(Font.custom("Avenir Next", size: 18).bold())
-                        .foregroundColor(.white)
-                        Spacer()
-                            .frame(width: 100, height: 25, alignment: .topLeading)
-                    }
-                }
+        ScrollView {
+            VStack(alignment: .center, spacing: 5) {
+                
                 
                 // Decoration
                 ZStack {
                     Rectangle()
-                        .fill(LinearGradient(gradient: Gradient(colors: [.white, .red, .blue]), startPoint: .top, endPoint: .bottom))
-                        .frame(width: 35, height: 250, alignment: .bottomTrailing)
-                    
-                    Text("KOREA")
-                        .rotationEffect(Angle(degrees: 90))
+                        .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                        .frame(height: 35, alignment: .topLeading)
+                    Text("ARCHERY ARCHERY ARCHERY")
                         .font(Font.custom("Avenir Next", size: 25).italic().bold())
                         .foregroundColor(.white)
                 }
-                .padding(.vertical, 10)
-                .frame(width: 110, height: 200, alignment: .bottom)
+                Spacer()
+                // Photo
+                ZStack {
+                    Circle()
+                        .fill(Color.black)
+                        .padding(20)
+                    
+                    Circle()
+                        .fill(Color.blue)
+                        .padding(40)
+                    
+                    Circle()
+                        .fill(Color.red)
+                        .padding(60)
+                    
+                    Image(stat.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .padding(80)
+                }
+                Spacer()
+                
+                HStack {
+                    ZStack {
+                        // Background
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.red)
+                            .frame(width: 350, height: 230, alignment: .topLeading)
+                        RoundedRectangle(cornerRadius: 25)
+                            .fill(Color.blue.opacity(0.55))
+                            .frame(width: 370, height: 250, alignment: .topLeading)
+                        // Stats
+                        VStack(alignment: .leading, spacing: 3) {
+                            Group {
+                                Text(stat.debutDate)
+                                Text(stat.age)
+                                Text(stat.bestScore)
+                                Text(stat.winningPercentage)
+                                Text(stat.worldRanking)
+                                Text(stat.averageScore)
+                            }
+                            .font(Font.custom("Avenir Next", size: 18).bold())
+                            .foregroundColor(.white)
+                            Spacer()
+                                .frame(width: 100, height: 25, alignment: .topLeading)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    // Decoration
+                    VStack {
+                        Spacer()
+                        
+                        ZStack {
+                            Rectangle()
+                                .fill(LinearGradient(gradient: Gradient(colors: [.white, .red, .blue]), startPoint: .top, endPoint: .bottom))
+                                .frame(width: 35, height: 250, alignment: .bottomTrailing)
+                            
+                            Text("KOREA")
+                                .rotationEffect(Angle(degrees: 90))
+                                .font(Font.custom("Avenir Next", size: 25).italic().bold())
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 110, height: 200, alignment: .bottom)
+                    }
+                }
             }
-            
-            // invisible rectangle
-            Rectangle()
-                .frame(width: 500, height: 90, alignment: .center)
+            .padding()
         }
+        .navigationTitle(stat.archerName)
     }
 }
 struct ContentView_Previews: PreviewProvider {
