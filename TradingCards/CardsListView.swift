94817fc86afa16fea685ea2d911a02071e7e28c2
lcs-ashin <ashin@lcs.on.ca>
2021-11-10 14:40:46 -0500

    Created list view ("CardsListView") and used the stored properties I made in the model page (Archer)

---
+++ TradingCards/CardsListView.swift
@@ -0,0 +1,47 @@
+//
+//  CardsListView.swift
+//  TradingCards
+//
+//  Created by Eunbi Shin on 2021-11-10.
+//
+
+import SwiftUI
+
+struct CardsListView: View {
+    let imageName: String
+    let archerName: String
+    
+    var body: some View {
+        List {
+            ForEach(listOfStats) { currentStat in
+                
+                NavigationLink(destination: {
+                    ContentView(stat: currentStat)
+                }, label: {
+                    HStack {
+                        Image(currentStat.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipShape(Circle())
+                            .clipped()
+                        VStack(alignment: .leading) {
+                            Text(currentStat.archerName)
+                                .bold()
+                        }
+                    }
+                })
+                
+            }
+        }
+        .navigationTitle("Archers")
+    }
+}
+
+struct CardsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CardsListView(imageName: "An San", archerName: "An San")
+        }
+    }
+}
