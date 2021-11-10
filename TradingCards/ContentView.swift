901d9edc4f19f1d90e424f754febf7cb074f5b60
lcs-ashin <ashin@lcs.on.ca>
2021-11-09 21:50:22 -0500

    Created a page of an archer's statistics using V/H/ZStacks. (made the basic layout for the trading cards)

--- TradingCards/ContentView.swift
+++ TradingCards/ContentView.swift
@@ -9,13 +9,94 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .center, spacing: 5) {
+            
+            // Invisible rectangle
+            Rectangle()
+                .frame(width: 500, height: 20, alignment: .center)
+            // Name
+            Text("An San")
+                .font(Font.custom("Helvetica Neue", size: 40).bold().italic())
+            // Decoration
+            ZStack {
+                Rectangle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: 450, height: 35, alignment: .topLeading)
+                Text("ARCHERY ARCHERY ARCHERY")
+                    .font(Font.custom("Avenir Next", size: 25).italic().bold())
+                    .foregroundColor(.white)
+            }
+            Spacer()
+            // Photo
+            ZStack {
+                Circle()
+                    .fill(Color.black)
+                    .frame(width: 390, height: 390, alignment: .center)
+                Circle()
+                    .fill(Color.blue)
+                    .frame(width: 370, height: 380, alignment: .center)
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 345, height: 370, alignment: .center)
+                Image("An San")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .padding()
+            }
+            Spacer()
+            
+            HStack {
+                ZStack {
+                    // Background
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.red)
+                        .frame(width: 350, height: 230, alignment: .topLeading)
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.blue.opacity(0.55))
+                        .frame(width: 370, height: 250, alignment: .topLeading)
+                    // Stats
+                    VStack(alignment: .leading, spacing: 3) {
+                        Group {
+                            Text("International Debut: 2019")
+                            Text("Age: 20")
+                            Text("Best Score: 684")
+                            Text("Winning Percentage: 76%")
+                            Text("World Ranking: 1")
+                            Text("Average Score per arrow: 9.4")
+                        }
+                        .font(Font.custom("Avenir Next", size: 18).bold())
+                        .foregroundColor(.white)
+                        Spacer()
+                            .frame(width: 100, height: 25, alignment: .topLeading)
+                    }
+                }
+                
+                // Decoration
+                ZStack {
+                    Rectangle()
+                        .fill(LinearGradient(gradient: Gradient(colors: [.white, .red, .blue]), startPoint: .top, endPoint: .bottom))
+                        .frame(width: 35, height: 250, alignment: .bottomTrailing)
+                    
+                    Text("KOREA")
+                        .rotationEffect(Angle(degrees: 90))
+                        .font(Font.custom("Avenir Next", size: 25).italic().bold())
+                        .foregroundColor(.white)
+                }
+                .padding(.vertical, 10)
+                .frame(width: 110, height: 200, alignment: .bottom)
+            }
+            
+            // invisible rectangle
+            Rectangle()
+                .frame(width: 500, height: 90, alignment: .center)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
